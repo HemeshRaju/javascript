baa1283815005e3a0d0bf32a7968f148f94fff9d
HemeshRaju <kapallihemeshraju@gmail.com>
2025-01-09 09:53:06 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,6 @@
+function name() {
+    a=parseInt(prompt())
+    b=parseInt(prompt())
+    console.log(a+b)
+}
+name()
---
+++ loop.js
@@ -0,0 +1,56 @@
+//while-loop
+// ->it will iterates set of instruction  
+/*
+while(condition)
+{
+set of statments
+
+}
+ */
+
+let i=1
+
+while(i<=5)
+{
+    console.log(i)
+    i++
+}
+// 1 2 3 4 5
+// do-while loop
+// -> it will execute and then checks condition
+// -> by default one iteration will be there
+/*
+do
+{
+   set-of-instruction
+
+   }
+   while(condition)
+ */
+   console.log('do while loop')
+
+let a=1
+
+do{
+    console.log(a);
+     a++
+}
+while(a<=5)
+// 1 2 3 4 5
+
+
+// for loop
+// -> it will execute set of instruction for a specific number of times
+
+/*
+for(initialization;condition;increment/decrement)
+{
+set of instruction
+}
+*/
+console.log(' for loop')
+for(let i=5;i>=1;i--)
+    {
+        console.log(i);
+
+    }
